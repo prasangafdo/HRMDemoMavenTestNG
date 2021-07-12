ce35cb553015e34d39c57fdbade075f3c5405c83
Prasanga Fernando <prasangafdz@outlook.com>
2021-07-12 16:15:33 +0530

    Fixed navigation issues in the JobCategories class

--- src/test/java/com/maven/testng/VerifyJobCategories.java
+++ src/test/java/com/maven/testng/VerifyJobCategories.java
@@ -1,13 +1,19 @@
 package com.maven.testng;
 
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class VerifyJobCategories extends BackgroundWorker{
 	
-	@BeforeTest
+	@BeforeClass
 	public void navigateToJobCategories() {
 		navigateToUserManagement();
 		selectJobCategories();
+	}
+	
+	@Test
+	public void test() {
+		//To run the before class method, we need to add at least one test method in TestNG
 	}
 
 }
