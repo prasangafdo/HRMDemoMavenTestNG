25469897a4b5641f02bc9acfa2fa74857e7dd3e1
Prasanga Fernando <prasangafdz@outlook.com>
2021-07-15 10:52:32 +0530

    Located elements in Add Work Shifts screen

--- src/test/java/com/maven/testng/VerifyWorkShifts.java
+++ src/test/java/com/maven/testng/VerifyWorkShifts.java
@@ -1,10 +1,12 @@
 package com.maven.testng;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class VerifyWorkShifts extends BackgroundWorker{
-	
+
 	@BeforeClass
 	public void navigateToWorkShifts() {
 		navigateToUserManagement();
@@ -13,7 +15,25 @@ public class VerifyWorkShifts extends BackgroundWorker{
 	
 	@Test
 	public void test1() {
+		driver.findElement(By.id("btnAdd")).click();
+		
+		//Hardcoding data for now just to verify the element locaters.
+		driver.findElement(By.id("workShift_name")).sendKeys("Test1");
+		
+		//Selecting work hours
+		Select hoursFrom = new Select(driver.findElement(By.id("workShift_workHours_from")));
+		hoursFrom.selectByValue("10:00");
+		
+		Select hoursTo = new Select(driver.findElement(By.id("workShift_workHours_to")));
+		hoursTo.selectByVisibleText("18:00");
+		
+		Select availableEmployee = new Select(driver.findElement(By.id("workShift_availableEmp")));
+		availableEmployee.selectByVisibleText("Fiona Grace");
+		
+		driver.findElement(By.xpath("//*/a[@id='btnAssignEmployee']")).click();
+		
 		
 	}
+	
 
 }
