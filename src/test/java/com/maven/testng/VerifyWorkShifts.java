77936d366e4f52a55ef86ca55ff8e726dd52ddc1
Prasanga Fernando <prasangafdz@outlook.com>
2021-07-17 19:46:30 +0530

    Minor changes

--- src/test/java/com/maven/testng/VerifyWorkShifts.java
+++ src/test/java/com/maven/testng/VerifyWorkShifts.java
@@ -2,6 +2,7 @@ package com.maven.testng;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -13,27 +14,39 @@ public class VerifyWorkShifts extends BackgroundWorker{
 		selectWorkShifts();
 	}
 	
-	@Test
-	public void test1() {
+	@Test(dataProvider="extractedWorkShifts", dataProviderClass=DataDriver.class)
+	public void test1(String shiftName, String hoursFrom, String hoursTo, String employee) {
 		driver.findElement(By.id("btnAdd")).click();
 		
 		//Hardcoding data for now just to verify the element locaters.
-		driver.findElement(By.id("workShift_name")).sendKeys("Test1");
+		driver.findElement(By.id("workShift_name")).sendKeys(shiftName);
 		
 		//Selecting work hours
-		Select hoursFrom = new Select(driver.findElement(By.id("workShift_workHours_from")));
-		hoursFrom.selectByValue("10:00");
+		Select ddl_hoursFrom = new Select(driver.findElement(By.id("workShift_workHours_from")));
+		ddl_hoursFrom.selectByValue(hoursFrom);
 		
-		Select hoursTo = new Select(driver.findElement(By.id("workShift_workHours_to")));
-		hoursTo.selectByVisibleText("18:00");
+		Select ddl_hoursTo = new Select(driver.findElement(By.id("workShift_workHours_to")));
+		ddl_hoursTo.selectByVisibleText(hoursTo);
 		
 		Select availableEmployee = new Select(driver.findElement(By.id("workShift_availableEmp")));
-		availableEmployee.selectByVisibleText("Fiona Grace");
+		availableEmployee.selectByVisibleText(employee);
 		
 		driver.findElement(By.xpath("//*/a[@id='btnAssignEmployee']")).click();
 		
 		
+		///////////
+		navigateToUserManagement();
+		selectWorkShifts();
+		
+		
 	}
+	
+	@AfterMethod
+	public void aaa() {
+		navigateToUserManagement();
+		selectWorkShifts();
+	}
+	
 	
 
 }
