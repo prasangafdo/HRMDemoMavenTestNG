8a45ff64e9687a1231c19716ee9fa018e5efd23d
Prasanga Fernando <prasangafdz@outlook.com>
2021-07-28 11:52:30 +0530

    Completed verifyAddSkill method
    Located elements but for now sending hard coded data till the data provider method get implemented

--- src/test/java/com/maven/testng/VerifyQualificationSkills.java
+++ src/test/java/com/maven/testng/VerifyQualificationSkills.java
@@ -1,13 +1,22 @@
 package com.maven.testng;
 
 import org.openqa.selenium.By;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class VerifyQualificationSkills extends BackgroundWorker{
 
-	@Test
-	public void aaa() {
+	@BeforeClass
+	public void navigateToSkills() {
 		selectSkills();
+	}
+	@Test
+	public void verifyAddSkill() {
+		
 		driver.findElement(By.id("btnAdd")).click();
+		
+		driver.findElement(By.id("skill_name")).sendKeys("TestSkill0001");
+		driver.findElement(By.id("skill_description")).sendKeys("Test Skill Description 0001");
+		driver.findElement(By.id("btnSave")).click();
 	}
 }
