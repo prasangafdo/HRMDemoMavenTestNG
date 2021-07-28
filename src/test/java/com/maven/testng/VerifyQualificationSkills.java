16978da1a36425b1869d99c78e12f0c177ec0986
Prasanga Fernando <prasangafdz@outlook.com>
2021-07-28 11:47:37 +0530

    Added navigation to add skills page

--- src/test/java/com/maven/testng/VerifyQualificationSkills.java
+++ src/test/java/com/maven/testng/VerifyQualificationSkills.java
@@ -1,5 +1,6 @@
 package com.maven.testng;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 public class VerifyQualificationSkills extends BackgroundWorker{
@@ -7,5 +8,6 @@ public class VerifyQualificationSkills extends BackgroundWorker{
 	@Test
 	public void aaa() {
 		selectSkills();
+		driver.findElement(By.id("btnAdd")).click();
 	}
 }
