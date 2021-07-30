5fc690f88a6ceb83b3884c038df4e4c7988eb395
Prasanga Fernando <prasangafdz@outlook.com>
2021-07-30 08:20:23 +0530

    Created new class and removed hard coded data
    Created VerifyQualificationEducation class.
    Removed hard coded data from VerifyQualificationSkills class

--- src/test/java/com/maven/testng/VerifyQualificationEducation.java
+++ src/test/java/com/maven/testng/VerifyQualificationEducation.java
@@ -4,13 +4,22 @@ import org.openqa.selenium.By;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class VerifyQualificationSkills extends BackgroundWorker{
+public class VerifyQualificationEducation extends BackgroundWorker{
 
 	@BeforeClass
 	public void navigateToSkills() {
 		selectSkills();
 	}
-
+//	@Test
+//	public void verifyAddSkill() {
+//		
+//		driver.findElement(By.id("btnAdd")).click();
+//		
+//		driver.findElement(By.id("skill_name")).sendKeys("TestSkill0001");
+//		driver.findElement(By.id("skill_description")).sendKeys("Test Skill Description 0001");
+//		driver.findElement(By.id("btnSave")).click();
+//	}
+//	
 	@Test(priority = 1, dataProvider ="extractedQualificationSkills", dataProviderClass = DataDriver.class)
 	public void verifyAddSkill(String skill, String description) {
 		driver.findElement(By.id("btnAdd")).click();
