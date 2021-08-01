6b0d7c027a729b47e4e406499aff557cde5585c6
Prasanga Fernando <prasangafdz@outlook.com>
2021-08-01 11:15:39 +0530

    Minor changes

--- src/test/java/com/maven/testng/VerifyQualificationEducation.java
+++ src/test/java/com/maven/testng/VerifyQualificationEducation.java
@@ -11,30 +11,14 @@ public class VerifyQualificationEducation extends BackgroundWorker{
 		selectEducation();
 	}
 	
-	@Test()
-	public void verifyAddEducation() {//Rename the method name later
+	String educationName = "Test Education name 0001"; //Change the variable value for the input if needed
+	
+	@Test
+	public void AddEducation() {//Since this test case has only one input field, I'm not going to use a data provider to test with different data.
 		driver.findElement(By.id("btnAdd")).click();
-		driver.findElement(By.id("education_name")).sendKeys("11");
+		driver.findElement(By.id("education_name")).sendKeys(educationName);
 		driver.findElement(By.id("btnSave")).click();
 		
 	}
 	
-//	@Test
-//	public void verifyAddSkill() {
-//		
-//		driver.findElement(By.id("btnAdd")).click();
-//		
-//		driver.findElement(By.id("skill_name")).sendKeys("TestSkill0001");
-//		driver.findElement(By.id("skill_description")).sendKeys("Test Skill Description 0001");
-//		driver.findElement(By.id("btnSave")).click();
-//	}
-//	
-//	@Test(priority = 1, dataProvider ="extractedQualificationSkills", dataProviderClass = DataDriver.class)
-//	public void verifyAddSkill(String skill, String description) {
-//		driver.findElement(By.id("btnAdd")).click();
-//		
-//		driver.findElement(By.id("skill_name")).sendKeys(skill);
-//		driver.findElement(By.id("skill_description")).sendKeys(description);
-//		driver.findElement(By.id("btnSave")).click();
-//	}
 }
