39f821289429cbe858585a40a4d6bd6cdba559e3
Prasanga Fernando <prasangafdz@outlook.com>
2021-08-06 15:16:43 +0530

    Minor changes

--- src/test/java/com/maven/testng/VerifyQualificationLanguages.java
+++ src/test/java/com/maven/testng/VerifyQualificationLanguages.java
@@ -1,11 +1,18 @@
 package com.maven.testng;
 
+import org.openqa.selenium.By;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class VerifyQualificationLanguages extends BackgroundWorker{
 
-	@Test
+	@BeforeClass
 	public void navigate() {
 		selectLanguages();
+	}
+	
+	@Test
+	public void VerifyAddLanguage() {//Since this test case has only one input field, I'm not going to use a data provider to test with different data.
+		driver.findElement(By.id("btnAdd")).click();
 	}
 }
