a5efcbe7776a8ab5002561a840f36f174a35540a
Prasanga Fernando <prasangafdz@outlook.com>
2021-08-07 20:16:09 +0530

    Completed VerifyAddLanguage method.

--- src/test/java/com/maven/testng/VerifyQualificationLanguages.java
+++ src/test/java/com/maven/testng/VerifyQualificationLanguages.java
@@ -11,8 +11,12 @@ public class VerifyQualificationLanguages extends BackgroundWorker{
 		selectLanguages();
 	}
 	
+	String LanguageName = "Test License name 0001"; //Change the variable value for the input if needed
+	
 	@Test
 	public void VerifyAddLanguage() {//Since this test case has only one input field, I'm not going to use a data provider to test with different data.
 		driver.findElement(By.id("btnAdd")).click();
+		driver.findElement(By.id("language_name")).sendKeys(LanguageName);
+		driver.findElement(By.id("btnSave")).click();
 	}
 }
