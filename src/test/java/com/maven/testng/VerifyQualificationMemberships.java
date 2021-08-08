59878acf5d56e97a04cbd61dc3f889bf2ce8c567
Prasanga Fernando <prasangafdz@outlook.com>
2021-08-08 10:46:54 +0530

    Completed VerifyAddMemberships method

--- src/test/java/com/maven/testng/VerifyQualificationMemberships.java
+++ src/test/java/com/maven/testng/VerifyQualificationMemberships.java
@@ -1,11 +1,22 @@
 package com.maven.testng;
 
+import org.openqa.selenium.By;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class VerifyQualificationMemberships extends BackgroundWorker{
 
+	@BeforeClass
+	public void navigate() {
+		selectMemberships();
+	}
+	
+	String MembershipName = "Test Membership name 0001"; //Change the variable value for the input if needed
+	
 	@Test
 	public void VerifyAddMemberships() {
-		selectMemberships();
+		driver.findElement(By.id("btnAdd")).click();
+		driver.findElement(By.id("membership_name")).sendKeys(MembershipName);
+		driver.findElement(By.id("btnSave")).click();
 	}
 }
