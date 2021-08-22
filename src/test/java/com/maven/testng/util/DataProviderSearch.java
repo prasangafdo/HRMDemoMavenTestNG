2afd8221c24c07bed099148fb909c9cb4cc9164e
Prasanga Fernando <prasangafdz@outlook.com>
2021-08-22 16:22:30 +0530

    Update DataProviderSearch.java

--- src/test/java/com/maven/testng/util/DataProviderSearch.java
+++ src/test/java/com/maven/testng/util/DataProviderSearch.java
@@ -1,5 +1,11 @@
 package com.maven.testng.util;
 
 public class DataProviderSearch {
+	/*Add a method to get the same data used in create methods.
+	 * Use same data and get the results after create methods have completed execution.
+	 * 
+	 */
+	
+	
 
 }
