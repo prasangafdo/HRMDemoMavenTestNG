37de01bf7aae3d8f3a7e173ab3ab9ea72458cdd8
Prasanga Fernando <prasangafdz@outlook.com>
2021-08-29 11:35:52 +0530

    Minor changes to SearchUserFunction class

--- src/test/java/com/maven/testng/function/SearchUserFunction.java
+++ src/test/java/com/maven/testng/function/SearchUserFunction.java
@@ -1,8 +1,20 @@
 package com.maven.testng.function;
 
 import com.maven.testng.BackgroundWorker;
+import com.maven.testng.page.UsersPage;
 
 public class SearchUserFunction extends BackgroundWorker{
+	
+	static UsersPage usersPage = new UsersPage();
 
+	public void searchWithUsernameAndAllUserTypes() {
+		usersPage.searchByUsername("aaaa"); //Data is hardcoded for now
+	}
+	public void searchWithUsernameAndAdminUserType() {
+		
+	}
+	public void searchWithUsernameAndESSUserType() {
+		
+	}
 	
 }
