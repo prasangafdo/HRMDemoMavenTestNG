f8856768e4f0992a0cf7b03974f46f9b594c01bd
Prasanga Fernando <prasangafdz@outlook.com>
2021-09-11 12:31:21 +0530

    Added element locators to "AddUserPage" class

--- src/test/java/com/maven/testng/page/AddUserPage.java
+++ src/test/java/com/maven/testng/page/AddUserPage.java
@@ -1,5 +1,20 @@
 package com.maven.testng.page;
 
-public class AddUserPage {
+import org.openqa.selenium.By;
 
+import com.maven.testng.BackgroundWorker;
+
+public class AddUserPage extends BackgroundWorker{
+	
+	private static By drpDwnUserRole = By.id("systemUser_userType");
+	private static By txtEmployeeName = By.id("systemUser_employeeName_empName");
+	private static By txtUsername = By.id("systemUser_userName");
+	private static By lblUsername = By.xpath("//div/ul/li[@class='ac_even ac_over']");
+	private static By drpDwnStatus = By.id("systemUser_status");
+	private static By txtPassword = By.id("systemUser_password");
+	private static By txtConfirmPassword = By.id("systemUser_confirmPassword");
+	private static By btnSave = By.id("btnSave");
+	private static By btnCancel = By.id("btnCancel");
+	
+	
 }
