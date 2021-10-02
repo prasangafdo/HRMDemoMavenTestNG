fa082bda56472e3d71b74119097bc081457fb456
Prasanga Fernando <prasangafdz@outlook.com>
2021-10-02 19:00:05 +0530

    Final commit on 02.10.2021
    Stopped working on test UI automation of this application

--- src/test/java/com/maven/testng/page/UsersPage.java
+++ src/test/java/com/maven/testng/page/UsersPage.java
@@ -16,7 +16,7 @@ public class UsersPage extends BrowserManager{
 	private static By drpDwnUserRole = By.id("searchSystemUser_userType");
 	private static By txtEmployeeName = By.name("searchSystemUser[employeeName][empName]");
 	private static By drpDwnStatus = By.id("searchSystemUser_status");
-	private static By drpDwnEmployeeName = By.xpath("//ul/li[@class='ac_even ac_over']"); //This element is a ul element.
+	private static By drpDwnEmployeeName = By.xpath("//ul/li[@class='ac_even ac_over']/parent::ul"); //This element is a ul element.
 	//private static By lblSearchResult = By.xpath("//td/a[text()='Garry.White']"); //Change this later
 	private static By lblSearchResult = By.name("chkSelectRow[]");//Change variable name later
 	private static By lblNoRecordsFound = By.xpath("//td[text()='No Records Found']");
